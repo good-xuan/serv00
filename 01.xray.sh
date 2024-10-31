5919351520ccc6bbdfe79f8e24ca339ddf853354
good-xuan <66776933+good-xuan@users.noreply.github.com>
2024-10-31 12:10:56 +0800

    Update 01.xray.sh

--- 01.xray.sh
+++ 01.xray.sh
@@ -3,6 +3,11 @@
 # 获取最新版本号
 latest_version=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
 
+# 如果没有获取到信息，设置默认值
+if [ -z "$latest_version" ]; then
+    latest_version="v24.9.30"
+fi
+
 # 设置下载链接
 wget  https://github.com/XTLS/Xray-core/releases/download/${latest_version}/Xray-freebsd-64.zip
 
