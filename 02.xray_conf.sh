b4ea8453caf485590956db6fd9ba3c3e3ad08f2b
good-xuan <66776933+good-xuan@users.noreply.github.com>
2024-11-10 14:24:55 +0800

    Update 02.xray_conf.sh

--- 02.xray_conf.sh
+++ 02.xray_conf.sh
@@ -21,86 +21,93 @@ fi
 
 # 创建配置文件内容
 cat <<EOF > $config_path
+
 {
-	"routing": {
-		"rules": [
-			{
-				"port": "443",
-				"network": "udp",
-				"outboundTag": "block"
-			}
-		]
-	},
-	"inbounds": [
-		{
-			"port": "$vless_port",
-			"protocol": "vless",
-			"settings": {
-				"clients": [
-					{
-						"id": "$uuid"
-					}
-				],
-				"decryption": "none"
-			},
-			"streamSettings": {
-				"sockopt": {
-					"tcpMptcp": true,
-					"tcpNoDelay": true
-				},
-				"network": "ws",
-				"security": "tls",
-				"tlsSettings": {
-					"alpn": [
-						"http/1.1"
-					],
-					"minVersion": "1.3",
-					"certificates": [
-						{
-							"certificate": [
-								"-----BEGIN CERTIFICATE-----",
-								"MIIBgDCCASagAwIBAgIRAKKibtLxdvJrh+oY/G3B4GYwCgYIKoZIzj0EAwIwJjER",
-								"MA8GA1UEChMIWHJheSBJbmMxETAPBgNVBAMTCFhyYXkgSW5jMB4XDTI0MTExMDA1",
-								"MTkyOFoXDTI1MDIwODA2MTkyOFowJjERMA8GA1UEChMIWHJheSBJbmMxETAPBgNV",
-								"BAMTCFhyYXkgSW5jMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEUZUR6KmqNSDE",
-								"3eo4RUmDzQSK7MoWoHbriTtxlP3atJKbL78/eOf1jlK901EQcujzANSFi3X/b3KV",
-								"ckcZVv5146M1MDMwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMB",
-								"MAwGA1UdEwEB/wQCMAAwCgYIKoZIzj0EAwIDSAAwRQIhAOg3ze6ngrwvhB0/wGBD",
-								"5QbZFmuQtm8mEanIMCcx4LX3AiAlEa6odTfTEeUwWm23PF/xOP4jr8spKq9m+1aJ",
-								"2VuE9A==",
-								"-----END CERTIFICATE-----"
-							],
-							"key": [
-								"-----BEGIN RSA PRIVATE KEY-----",
-								"MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgNeiiFrdxbipL3uIe",
-								"ehNIKkVV8cD/9X1RRQKb0uSE/DmhRANCAARRlRHoqao1IMTd6jhFSYPNBIrsyhag",
-								"duuJO3GU/dq0kpsvvz945/WOUr3TURBy6PMA1IWLdf9vcpVyRxlW/nXj",
-								"-----END RSA PRIVATE KEY-----"
-							]
-						}
-					]
-				}
-			},
-			"sniffing": {
-				"enabled": true,
-				"destOverride": [
-					"http",
-					"tls",
-					"quic"
-				]
-			}
-		}
-	],
-	"outbounds": [
-		{
-			"protocol": "freedom",
-			"tag": "direct"
-		},
-		{
-			"protocol": "blackhole",
-			"tag": "block"
-		}
-	]
+  "routing":
+  {
+    "rules": [
+      {
+        "port": "443",
+        "network": "udp",
+        "outboundTag": "block"
+      }
+    ]
+  },
+  "inbounds": [
+    {
+      "port": "$vless_port",
+      "protocol": "vless",
+      "settings":
+      {
+        "clients": [
+          {
+            "id": "$uuid"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings":
+      {
+        "sockopt":
+        {
+          "tcpMptcp": true,
+          "tcpNoDelay": true
+        },
+        "network": "ws",
+        "security": "tls",
+        "tlsSettings":
+        {
+          "alpn": [
+            "http/1.1"
+          ],
+          "minVersion": "1.3",
+          "certificates": [
+            {
+              "certificate": [
+                "-----BEGIN CERTIFICATE-----",
+                "MIIBgDCCASagAwIBAgIRAKKibtLxdvJrh+oY/G3B4GYwCgYIKoZIzj0EAwIwJjER",
+                "MA8GA1UEChMIWHJheSBJbmMxETAPBgNVBAMTCFhyYXkgSW5jMB4XDTI0MTExMDA1",
+                "MTkyOFoXDTI1MDIwODA2MTkyOFowJjERMA8GA1UEChMIWHJheSBJbmMxETAPBgNV",
+                "BAMTCFhyYXkgSW5jMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEUZUR6KmqNSDE",
+                "3eo4RUmDzQSK7MoWoHbriTtxlP3atJKbL78/eOf1jlK901EQcujzANSFi3X/b3KV",
+                "ckcZVv5146M1MDMwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMB",
+                "MAwGA1UdEwEB/wQCMAAwCgYIKoZIzj0EAwIDSAAwRQIhAOg3ze6ngrwvhB0/wGBD",
+                "5QbZFmuQtm8mEanIMCcx4LX3AiAlEa6odTfTEeUwWm23PF/xOP4jr8spKq9m+1aJ",
+                "2VuE9A==",
+                "-----END CERTIFICATE-----"
+              ],
+              "key": [
+                "-----BEGIN RSA PRIVATE KEY-----",
+                "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgNeiiFrdxbipL3uIe",
+                "ehNIKkVV8cD/9X1RRQKb0uSE/DmhRANCAARRlRHoqao1IMTd6jhFSYPNBIrsyhag",
+                "duuJO3GU/dq0kpsvvz945/WOUr3TURBy6PMA1IWLdf9vcpVyRxlW/nXj",
+                "-----END RSA PRIVATE KEY-----"
+              ]
+            }
+          ]
+        }
+      },
+      "sniffing":
+      {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls",
+          "quic"
+        ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "tag": "block"
+    }
+  ]
 }
 EOF
 
