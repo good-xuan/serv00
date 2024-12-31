42efb3a57f0449208ae6bc1be1a82ef839a4430e
good-xuan <66776933+good-xuan@users.noreply.github.com>
2024-12-31 11:34:54 +0800

    Update 02.xray_conf.sh

--- 02.xray_conf.sh
+++ 02.xray_conf.sh
@@ -60,36 +60,7 @@ cat <<EOF > $config_path
           "tcpMptcp": true,
           "tcpNoDelay": true
         },
-        "network": "ws",
-        "security": "tls",
-        "tlsSettings":
-        {
-          "minVersion": "1.3",
-          "certificates": [
-            {
-              "certificate": [
-                "-----BEGIN CERTIFICATE-----",
-                "MIIBgDCCASagAwIBAgIRAKKibtLxdvJrh+oY/G3B4GYwCgYIKoZIzj0EAwIwJjER",
-                "MA8GA1UEChMIWHJheSBJbmMxETAPBgNVBAMTCFhyYXkgSW5jMB4XDTI0MTExMDA1",
-                "MTkyOFoXDTI1MDIwODA2MTkyOFowJjERMA8GA1UEChMIWHJheSBJbmMxETAPBgNV",
-                "BAMTCFhyYXkgSW5jMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEUZUR6KmqNSDE",
-                "3eo4RUmDzQSK7MoWoHbriTtxlP3atJKbL78/eOf1jlK901EQcujzANSFi3X/b3KV",
-                "ckcZVv5146M1MDMwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMB",
-                "MAwGA1UdEwEB/wQCMAAwCgYIKoZIzj0EAwIDSAAwRQIhAOg3ze6ngrwvhB0/wGBD",
-                "5QbZFmuQtm8mEanIMCcx4LX3AiAlEa6odTfTEeUwWm23PF/xOP4jr8spKq9m+1aJ",
-                "2VuE9A==",
-                "-----END CERTIFICATE-----"
-              ],
-              "key": [
-                "-----BEGIN RSA PRIVATE KEY-----",
-                "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgNeiiFrdxbipL3uIe",
-                "ehNIKkVV8cD/9X1RRQKb0uSE/DmhRANCAARRlRHoqao1IMTd6jhFSYPNBIrsyhag",
-                "duuJO3GU/dq0kpsvvz945/WOUr3TURBy6PMA1IWLdf9vcpVyRxlW/nXj",
-                "-----END RSA PRIVATE KEY-----"
-              ]
-            }
-          ]
-        }
+        "network": "ws"
       },
       "sniffing":
       {
