0c3a97e8d87b37ddf24080987cdcf332845a8035
good-xuan <66776933+good-xuan@users.noreply.github.com>
2025-02-01 10:55:03 +0800

    Update 01.xray.sh

--- 01.xray.sh
+++ 01.xray.sh
@@ -5,7 +5,7 @@ latest_version=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/la
 
 # 如果没有获取到信息，设置默认值
 if [ -z "$latest_version" ]; then
-    latest_version="v24.12.31"
+    latest_version="v25.1.30"
 fi
 
 # 下载
