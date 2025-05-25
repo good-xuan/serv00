c97a5693773d0be39988ca1a930bfa82506e062d
good-xuan <66776933+good-xuan@users.noreply.github.com>
2025-05-25 10:13:34 +0800

    Update linux_xray.sh

--- linux_xray.sh
+++ linux_xray.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # 获取最新版本号
-latest_version=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
+latest_version=$(curl -Ls https://data.jsdelivr.com/v1/package/gh/XTLS/Xray-core | grep -Eo '"[0-9.]+",' | sed -n 1p | tr -d '",')
 
 # 如果没有获取到信息，设置默认值
 if [ -z "$latest_version" ]; then
-    latest_version="v25.2.21"
+    latest_version="25.2.21"
 fi
 
 # 下载
 cd /tmp
-wget  https://github.com/XTLS/Xray-core/releases/download/${latest_version}/Xray-linux-64.zip
-
-# 创建目标目录
-
-
+wget  https://github.com/XTLS/Xray-core/releases/download/v${latest_version}/Xray-linux-64.zip
 unzip -o Xray-linux-64.zip -d /opt/xray
 
 
@@ -22,5 +18,6 @@ unzip -o Xray-linux-64.zip -d /opt/xray
 # 清理下载的 zip 文件
 rm /tmp/Xray-linux-64.zip*
 
-systemctl  restart  xray
+pm2 restart xray
+#systemctl  restart  xray
 echo "Xray-core 已下载并重启xray"
